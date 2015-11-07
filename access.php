54dc1a3ee54d8009fc49eb241e935a1980f32d7c
mdc <mdc@serverbox.ch>
2015-11-07 20:19:03 +0100

    test env

--- access.php
+++ access.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 header("Content-Type: text/plain");
+echo getenv("USER")."\n";
+echo getenv("PASS");
+
 if($_POST["user"] === getenv("USER") && $_POST["pass"] === getenv("PASS")){
 	$_SESSION["login"] = true;
 	echo "ok";	
