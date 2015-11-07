b2ddcd8815da0b2256fa153828e66f4e4369f323
mdc <mdc@serverbox.ch>
2015-11-07 20:23:35 +0100

    changed env param

--- access.php
+++ access.php
@@ -1,10 +1,8 @@
 <?php
 session_start();
 header("Content-Type: text/plain");
-echo getenv("USER")."\n";
-echo getenv("PASS");
 
-if($_POST["user"] === getenv("USER") && $_POST["pass"] === getenv("PASS")){
+if($_POST["user"] === getenv("BUSER") && $_POST["pass"] === getenv("PASS")){
 	$_SESSION["login"] = true;
 	echo "ok";	
 	exit(0);
